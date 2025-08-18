bfd9c4f714b576ad3d828ce4fec25f804935037a
UBA <mac@UBA-2.local>
2025-08-18 15:38:51 +0100

    commit

--- model/student.js
+++ model/student.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 // Define the blueprint of a Student document
 const studentSchema = new mongoose.Schema(
@@ -22,11 +23,23 @@ const studentSchema = new mongoose.Schema(
       type: Date,
       required: true,
     },
+    password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 8,
+  },
   },
   // Auto adds createdAt & updatedAt
   { timestamps: true }
 ); 
-
+// Hash the password before saving the student document
+studentSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
 // Create the model (students collection)
 const Student = mongoose.model("Student", studentSchema);
 
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-// import Student from "../model/student.js";
 import {
   createStudent,
   getStudents,
@@ -26,61 +25,4 @@ pageRouter.get("/students/:id", getStudentById);
 pageRouter.put("/students/:id", updateStudent);
 pageRouter.delete("/students/:id", deleteStudent);
 
-// Route to add a new student
-// pageRouter.post("/students", async (req, res) => {
-//   try {
-//     const saveStudent = new Student(req.body);
-//     await saveStudent.save(); // Save to DB
-//     res.status(201).json(saveStudent);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
-
-// // READ ALL -  to get all students
-// pageRouter.get("/students", async (req, res) => {
-//   try {
-//     const students = await Student.find();
-//     res.json(students);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// // READ ONE - Get a student by ID
-// pageRouter.get("/students/:id", async (req, res) => {
-//   try {
-//     const student = await Student.findById(req.params.id);
-//     if(!student) return res.status(404).json({message:"Student not found"});
-//     res.json(student);
-//   } catch (error) {
-//     res.status(500).json({error: error.message});
-//   }
-// });
-
-// // UPDATE - Edit student by ID
-// pageRouter.put("/students/:id", async (req, res) => {
-//   try {
-//     const student = await Student.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       // return updated doc & validate
-//       { new: true, runValidators: true }
-//     );
-//     if (!student) return res.status(404).json({ message: "Student not found" });
-//     res.json(student);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
-
-// // DELETE - Remove a student by ID
-// pageRouter.delete("/students/:id", async (req, res) => {
-//   try {
-//     const student = await Student.findByIdAndDelete(req.params.id);
-//     if (!student) return res.status(404).json({ message: "Student not found" });
-//     res.json({ message: "Student deleted successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 export default pageRouter;
